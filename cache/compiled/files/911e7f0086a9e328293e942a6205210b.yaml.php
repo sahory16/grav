e456c657f2d8bc33f2284a04bff4928313dff329
root <root@grav.sahorymaila.com>
2019-07-10 22:18:19 +0000

    Realidad Aumentada: Marcadores

--- cache/compiled/files/911e7f0086a9e328293e942a6205210b.yaml.php
+++ cache/compiled/files/911e7f0086a9e328293e942a6205210b.yaml.php
@@ -2,9 +2,9 @@
 return [
     '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
     'filename' => '/var/www/html/grav/user/data/feed/baf8987f6c2b17e4ebd619aa116ad037.yaml',
-    'modified' => 1562192308,
+    'modified' => 1562595027,
     'data' => [
-        'last_checked' => 1562192308,
+        'last_checked' => 1562595027,
         'data' => [
             0 => [
                 'title' => 'Grav 1.6 Released!',
@@ -16,13 +16,13 @@ return [
                 'title' => 'Important Theme Updates',
                 'url' => 'https://getgrav.org/blog/important-theme-updates',
                 'date' => 1553169300,
-                'nicetime' => '3 meses antes'
+                'nicetime' => '4 meses antes'
             ],
             2 => [
                 'title' => 'All RocketTheme Grav Themes 50% Off for Black Friday',
                 'url' => 'https://getgrav.org/blog/rockettheme-blackfriday-2018',
                 'date' => 1542790800,
-                'nicetime' => '7 meses antes'
+                'nicetime' => '8 meses antes'
             ],
             3 => [
                 'title' => 'We\'re Moving Chat from Slack to Discord',
---
+++ user/pages/01.blog/realidad-aumentada-marcadores/item.es.md
@@ -0,0 +1,5 @@
+---
+title: 'Realidad Aumentada: Marcadores'
+---
+
+Camara
