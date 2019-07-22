f185a2e68b847d433906841e1672446626471ae3
sahory16 <saho.1606@gmail.com>
2019-07-22 15:48:40 -0500

    Revert "Merge branch 'master' of https://github.com/sahory16/grav"
    This reverts commit f85e8408bcf9ac8c7bf38164c685eb6fc92601f1, reversing
    changes made to 7114f46ef58c0d5f085d210da674249a35db40d5.

--- user/pages/01.blog/realidad-aumentada-marcadores/db_conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Pre-define connection to the MySQL database, please specify these fields based on your database configuration.*/
-  define('DBHOST', 'localhost');
-  define('DBDATA', 'realidad');
-  define('DBUSER', 'root');
-  define('DBPASS', '12345');
-?>
